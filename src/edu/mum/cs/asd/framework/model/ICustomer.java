6778f71b5bff66bd9da552317c499b6336fd629c
soerian <soerian@gmail.com>
2014-04-22 11:03:41 -0500

    Pushed methods in Customer to ICustomer
    Also updated diagram

--- src/edu/mum/cs/asd/framework/model/ICustomer.java
+++ src/edu/mum/cs/asd/framework/model/ICustomer.java
@@ -1,4 +1,10 @@
 package edu.mum.cs.asd.framework.model;
 
+import java.util.List;
+
 public interface ICustomer {
+	public void addAccount(IAccount account);
+	public void removeAccount(IAccount account);
+	public List<IAccount> getAccounts();
+	public void sendEmail(String email);
 }
