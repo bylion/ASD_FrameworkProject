1cb06a4d0639d4cbeae8d16c3e2c6412d86a6bdb
soerian <soerian@gmail.com>
2014-04-22 11:47:02 -0500

    Implemented Factory

--- src/edu/mum/cs/asd/framework/model/IFactory.java
+++ src/edu/mum/cs/asd/framework/model/IFactory.java
@@ -1,10 +1,12 @@
 package edu.mum.cs.asd.framework.model;
 
+import java.util.Map;
+
 import edu.mum.cs.asd.framework.model.command.ICommand;
 
 public interface IFactory {	
 	
-	public ICustomer createCustomer();
+	public ICustomer createCustomer(Map<String, String> attributes);
     public IAccount createAccount();
     public IEntry createEntry(IAccount account, double amount);
     public ICommand createTransaction(IAccount account, double amount);
