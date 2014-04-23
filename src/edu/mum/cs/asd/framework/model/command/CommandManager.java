ef8e64a9bd2417364611762d97b986183bc9ce9e
soerian <soerian@gmail.com>
2014-04-23 09:31:41 -0500

    Updated diagram and CommandManager with redo()

--- src/edu/mum/cs/asd/framework/model/command/CommandManager.java
+++ src/edu/mum/cs/asd/framework/model/command/CommandManager.java
@@ -5,23 +5,32 @@ import java.util.Stack;
 public class CommandManager {
 
     private Stack<ICommand> commandsExecuted;
+    private Stack<ICommand> commandsUndone;
     
     public CommandManager() {
     	commandsExecuted = new Stack<>();
+    	commandsUndone = new Stack<>();
     }
 
     public void submit(ICommand command) {
     	commandsExecuted.push(command);
+    	commandsUndone.clear();
     	command.execute();
     }
 
     public void undo() {
     	if (!commandsExecuted.isEmpty()) {
-    		commandsExecuted.pop().undo();
+    		ICommand cmd = commandsExecuted.pop();
+    		commandsUndone.push(cmd);
+    		cmd.undo();
     	}
     }
     
     public void redo(){
-        
+    	if (!commandsUndone.isEmpty()) {
+    		ICommand cmd = commandsUndone.pop();
+    		commandsExecuted.push(cmd);
+    		cmd.execute();
+    	}
     }
 }
